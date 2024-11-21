0e02b893dcf38ff2077da36d24b0a70e81703c60
bconnor22 <112410045+bconnor22@users.noreply.github.com>
2024-11-21 17:21:11 -0500

    comit

--- Chess2.java
+++ Chess2.java
@@ -43,5 +43,12 @@ public class Chess2 {
                 }
             }
         }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
